0b99a549d38dbc8a142eea97add99be97648a7b3
Léu Almeida <leo@webid.net.br>
2020-06-17 12:55:09 -0300

    feat(app): initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <h1> Hello World</h1>
+      <GlobalStyles />
+    </>
   );
 }
 
