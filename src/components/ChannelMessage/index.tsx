7eb74c9c2d4b68a65fea7d1485ce6d2de26cf478
Léu Almeida <leo@webid.net.br>
2020-06-19 17:52:03 -0300

    feat(ChannelMessage): create component

---
+++ src/components/ChannelMessage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+const ChannelMessage: React.FC = () => {
+  return (
+    <Container>
+     
+    </Container>
+  );
+};
+
+export default ChannelMessage;
