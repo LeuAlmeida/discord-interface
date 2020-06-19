e205bee057811f7d079924743cf849e94fff2d4b
Léu Almeida <leo@webid.net.br>
2020-06-19 19:02:16 -0300

    style(ChannelMessage): implementing stylesheet

--- src/components/ChannelMessage/styles.ts
+++ src/components/ChannelMessage/styles.ts
@@ -1,5 +1,100 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+import avatars from "../../static/avatars";
 
+interface AvatarProps {
+  src?: string;
+  isBot?: boolean;
+}
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  padding: 4px 16px;
+  margin-right: 4px;
+
+  background-color: transparent;
+
+  &.mention {
+    background-color: var(--mention-message);
+
+    border-left: 2px solid var(--mention-detail);
+    padding-left: 14px;
+  }
+
+  & + div {
+    margin-top: 13px;
+  }
+`;
+
+export const Avatar = styled.div<AvatarProps>`
+  flex-shrink: 0;
+
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+
+  background: var(--primary)
+    ${(props) =>
+      props.src ? `url('${props.src}')` : `url('${avatars.default}')`} center;
+  background-size: 32px 32px;
+
+  &.bot {
+    background: var(--primary) url('${avatars.bot}') center;
+    background-size: 32px 32px;
+  }
+`;
+
+export const Message = styled.div`
+  min-height: 40px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 17px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+
+  > strong {
+    color: var(--white);
+    font-size: 16px;
+  }
+
+  > span {
+    margin-left: 6px;
+
+    background-color: var(--discord);
+    color: var(--white);
+    border-radius: 4px;
+    padding: 4px 5px;
+
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 11px;
+  }
+
+  > time {
+    margin-left: 6px;
+    color: var(--gray);
+    font-size: 13px;
+  }
+`;
+
+export const Content = styled.div`
+  text-align: left;
+  font-size: 16px;
+  color: var(--white);
+`;
+
+export const Mention = styled.span`
+  color: var(--link);
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
