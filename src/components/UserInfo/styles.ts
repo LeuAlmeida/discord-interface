42a2674b0997bfb4dec13432d345a60e8d698b75
Léu Almeida <leo@webid.net.br>
2020-06-19 16:31:35 -0300

    style(UserInfo): update initial structure

--- src/components/UserInfo/styles.ts
+++ src/components/UserInfo/styles.ts
@@ -1,33 +1,18 @@
 import styled from "styled-components";
+import { Mic, Headset, Settings } from "styled-icons/material";
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
-`;
+export const Profile = styled.div``;
 
-export const Profile = styled.div`
+export const Avatar = styled.div``;
 
-`;
+export const UserData = styled.div``;
 
-export const Avatar = styled.div`
+export const Icons = styled.div``;
 
-`;
+export const MicIcon = styled(Mic)``;
 
-export const UserData = styled.div`
+export const HeadphoneIcon = styled(Headset)``;
 
-`;
-
-export const Icons = styled.div`
-
-`;
-
-export const MicIcon = styled.div`
-
-`;
-
-export const HeadphoneIcon = styled.div`
-
-`;
-
-export const SettingsIcon = styled.div`
-
-`;
+export const SettingsIcon = styled(Settings)``;
