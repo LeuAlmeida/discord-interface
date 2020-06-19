71172230364d512ddbdcdd4735d12695f0f0f1f7
Léu Almeida <leo@webid.net.br>
2020-06-19 15:32:31 -0300

    feat(ChannelInfo): create component

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        Servidor do Léu
+      </Title>
+      
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ChannelInfo;
