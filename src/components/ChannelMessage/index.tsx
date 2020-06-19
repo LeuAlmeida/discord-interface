0f4d46aa6338d6c2a24722030eac6d1951c24607
Léu Almeida <leo@webid.net.br>
2020-06-19 19:03:29 -0300

    feat(ChannelMessage): implementing functionalities to component

--- src/components/ChannelMessage/index.tsx
+++ src/components/ChannelMessage/index.tsx
@@ -1,11 +1,38 @@
 import React from "react";
 
-import { Container, Title, ExpandIcon } from "./styles";
+import { Container, Avatar, Message, Header, Content } from "./styles";
 
-const ChannelMessage: React.FC = () => {
+import avatars from "../../static/avatars";
+export { Mention } from "./styles";
+
+export interface MessageProps {
+  author: string;
+  date: string;
+  content: string | React.ReactElement | React.ReactNode;
+  hasMention?: boolean;
+  isBot?: boolean;
+}
+
+const ChannelMessage: React.FC<MessageProps> = ({
+  author,
+  date,
+  content,
+  hasMention,
+  isBot,
+}) => {
   return (
-    <Container>
-     
+    <Container className={hasMention ? "mention" : ""}>
+      <Avatar src={avatars.LeuAlmeida} className={isBot ? "bot" : ""} />
+      <Message>
+        <Header>
+          <strong>{author}</strong>
+
+          {isBot && <span>Bot</span>}
+
+          <time>{date}</time>
+        </Header>
+        <Content>{content}</Content>
+      </Message>
     </Container>
   );
 };
