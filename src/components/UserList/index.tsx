ba165ee0e474b664c4666b448a8a346ec88efbb6
Léu Almeida <leo@webid.net.br>
2020-06-19 17:07:07 -0300

    feat(UserList): implementing initial structure

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 
-import { Container, Title, ExpandIcon } from "./styles";
+import { Container, Role, User, Avatar } from "./styles";
 
-const ServerName: React.FC = () => {
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+  return (
+    <User>
+      <Avatar />
+
+      <strong>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+};
+
+const UserList: React.FC = () => {
   return (
     <Container>
-      <Title>Servidor do Léu</Title>
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="Léu Almeida" />
 
-      <ExpandIcon />
+      <Role>Offline - 18</Role>
+      <UserRow nickname="Diego Fernandes" isBot />
     </Container>
   );
 };
 
-export default ServerName;
+export default UserList;
