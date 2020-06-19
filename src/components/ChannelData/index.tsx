88bad72d7bc473a6f2b6a5d49a81a3793b4e2e66
Léu Almeida <leo@webid.net.br>
2020-06-19 19:02:52 -0300

    feat(ChannelData): implementing functionalities and prevent errors

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+
+import ChannelMessage, { Mention } from "../ChannelMessage";
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Léu Almeida"
+            date="18/07/2020"
+            content="Hoje é o meu aniversário!"
+          />
+        ))}
+
+        <ChannelMessage
+          hasMention
+          author="Léu Almeida"
+          date="18/07/2020"
+          content={
+            <>
+              <Mention>@Léu Almeida</Mention>, tudo bem?
+            </>
+          }
+        />
+
+        <ChannelMessage
+          author="Léu Almeida"
+          date="18/07/2020"
+          content="Hoje é o meu aniversário!"
+        />
+
+        <ChannelMessage
+          hasMention
+          isBot
+          author="Botzão Legalzão"
+          date="18/07/2020"
+          content={
+            <>
+              Oi <Mention>@Léu Almeida</Mention>, eu sou um bot
+            </>
+          }
+        />
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre" />
