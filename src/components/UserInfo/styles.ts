7640a707a8b8ecacf7763c430982380448fd4b45
Léu Almeida <leo@webid.net.br>
2020-06-19 16:56:09 -0300

    style(UserInfo): implementing styles

--- src/components/UserInfo/styles.ts
+++ src/components/UserInfo/styles.ts
@@ -1,18 +1,104 @@
 import styled from "styled-components";
 import { Mic, Headset, Settings } from "styled-icons/material";
 
-export const Container = styled.div``;
+interface Props {
+  src?: string;
+}
 
-export const Profile = styled.div``;
+export const Container = styled.div`
+  grid-area: UI;
 
-export const Avatar = styled.div``;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-export const UserData = styled.div``;
+  padding: 10px;
+  background-color: var(--quaternary);
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+`;
 
-export const Icons = styled.div``;
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-export const MicIcon = styled(Mic)``;
+export const Avatar = styled.div<Props>`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
 
-export const HeadphoneIcon = styled(Headset)``;
+  background: var(--gray) ${(props) => props.src && `url('${props.src}')`};
+  background-size: 32px 32px;
+`;
 
-export const SettingsIcon = styled(Settings)``;
+export const UserData = styled.div`
+  margin-left: 8px;
+
+  display: flex;
+  flex-direction: column;
+
+  > strong {
+    font-size: 13px;
+    color: var(--white);
+    display: block;
+  }
+
+  > span {
+    color: var(--gray);
+    font-size: 13px;
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+
+  > svg:not(:first-child) {
+    margin-left: 7px;
+  }
+`;
+
+export const MicIcon = styled(Mic)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--white);
+  opacity: 0.7;
+  cursor: pointer;
+
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 100%;
+  }
+`;
+
+export const HeadphoneIcon = styled(Headset)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--white);
+  opacity: 0.7;
+  cursor: pointer;
+
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 100%;
+  }
+`;
+
+export const SettingsIcon = styled(Settings)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--white);
+  opacity: 0.7;
+  cursor: pointer;
+
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 100%;
+  }
+`;
