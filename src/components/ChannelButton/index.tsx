202a384273b669b90562b132ff4f800ac241466f
Léu Almeida <leo@webid.net.br>
2020-06-19 15:57:50 -0300

    feat(ChannelButton): create component

---
+++ src/components/ChannelButton/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+const ChannelButton: React.FC = () => {
+  return (
+    <Container>
+      <Title>Servidor do Léu</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ChannelButton;
