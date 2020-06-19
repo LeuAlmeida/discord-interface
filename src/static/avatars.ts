fe2770bfb4b2fffc8b8fc4ffc4c9458a826d10e1
Léu Almeida <leo@webid.net.br>
2020-06-19 19:01:53 -0300

    feat(static/avatars): implementing more avatars

--- src/static/avatars.ts
+++ src/static/avatars.ts
@@ -1,6 +1,11 @@
+import discordLogo from "../assets/discord-logo.png";
+import defaultIcon from "../assets/default-icon.png";
+
 export default {
   bot:
     "https://lh3.googleusercontent.com/ZoDYlydaoXDuPMj17uC_WCozWZKyPOQemOfCHP9t8db3zbBMB4vN0O8-_AzBMKwkPKaI",
   LeuAlmeida:
     "https://avatars1.githubusercontent.com/u/42948574?s=460&u=cf88494dc9252d1a4dcb15268def6abc58365f69&v=4",
+  discord: discordLogo,
+  default: defaultIcon,
 };
