ad1222e075874608f8fd8fe08985994ce474380a
Léu Almeida <leo@webid.net.br>
2020-06-19 17:25:45 -0300

    feat(UserList): improving User List component

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 import { Container, Role, User, Avatar } from "./styles";
+import avatars from "../../static/avatars";
 
 interface UserProps {
   nickname: string;
   isBot?: boolean;
+  avatar?: string;
 }
 
-const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+const UserRow: React.FC<UserProps> = ({ nickname, isBot, avatar }) => {
   return (
     <User>
-      <Avatar />
+      <Avatar src={avatar || ''} />
 
       <strong>{nickname}</strong>
 
@@ -23,10 +25,10 @@ const UserList: React.FC = () => {
   return (
     <Container>
       <Role>Disponível - 1</Role>
-      <UserRow nickname="Léu Almeida" />
+      <UserRow nickname="Léu Almeida" avatar={avatars.LeuAlmeida} />
 
       <Role>Offline - 18</Role>
-      <UserRow nickname="Diego Fernandes" isBot />
+      <UserRow nickname="Diego Fernandes" isBot avatar={avatars.bot} />
     </Container>
   );
 };
