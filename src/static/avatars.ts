66081eb97ae29b5de71e6613378d5d6c16510dfc
Léu Almeida <leo@webid.net.br>
2020-06-19 17:14:41 -0300

    feat(static): create avatars file

---
+++ src/static/avatars.ts
@@ -0,0 +1,6 @@
+export default {
+  bot:
+    "https://lh3.googleusercontent.com/ZoDYlydaoXDuPMj17uC_WCozWZKyPOQemOfCHP9t8db3zbBMB4vN0O8-_AzBMKwkPKaI",
+  LeuAlmeida:
+    "https://avatars1.githubusercontent.com/u/42948574?s=460&u=cf88494dc9252d1a4dcb15268def6abc58365f69&v=4",
+};
