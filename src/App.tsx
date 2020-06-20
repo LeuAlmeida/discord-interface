535c7f738b4fd815a5270d152c6e9226bbe528c8
LeuAlmeida <leo@webid.net.br>
2020-06-20 12:50:48 -0300

    feat(App): implementing GithubCorner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import GithubCorner from 'react-github-corner';
 
 import Layout from "./components/Layout";
 import GlobalStyles from "./styles/GlobalStyles";
 
+import './App.css';
+
 function App() {
   return (
     <>
+      <GithubCorner
+        className="github-corner"
+        href="https://github.com/leualmeida/discord-interface"
+        bannerColor="#6e86d6"
+        octoColor="#272727"
+      />
       <Layout />
       <GlobalStyles />
     </>
