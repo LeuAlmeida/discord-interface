bf9aee68d3d8ddb9a8a48b52926ca1bca4256d92
Léu Almeida <leo@webid.net.br>
2020-06-22 15:28:14 -0300

    feat(ErrorScreen): implementing informations to Error Screen

--- src/components/ErrorScreen/index.tsx
+++ src/components/ErrorScreen/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { Container, Emoji, Warning, Description, Button } from "./styles";
+
 const ErrorScreen = () => {
-  return <h1>Erro!</h1>;
+  return (
+    <Container>
+      <Emoji />
+      <Warning>I'm sorry for this.</Warning>
+      <Description>Unfortunately this application was developed only for desktop, for study purposes.</Description>
+      <Button href="https://github.com/LeuAlmeida">
+        <span>My GitHub</span>
+      </Button>
+    </Container>
+  );
 };
 
 export default ErrorScreen;
