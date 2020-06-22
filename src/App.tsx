5786a369c1ddcc71b1ec5fa74210bdcfb8900ec8
Léu Almeida <leo@webid.net.br>
2020-06-22 15:08:44 -0300

    feat: create Error Screen to mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import { isMobile, isTablet } from "mobile-device-detect";
+
 import Layout from "./components/Layout";
+import ErrorScreen from "./components/ErrorScreen";
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
-      <Layout />
+      {isMobile || isTablet ? <ErrorScreen /> : <Layout />}
       <GlobalStyles />
     </>
   );
---
+++ src/components/ErrorScreen/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorScreen = () => {
+  return <h1>Erro!</h1>;
+};
+
+export default ErrorScreen;
